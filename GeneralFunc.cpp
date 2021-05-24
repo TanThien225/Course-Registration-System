150de2acb7a1142f8afbdea5ff50db3158f23b37
Nguyễn Tấn Thiện <tanthien225@gmail.com>
2021-05-24 23:08:41 +0700

    Update GeneralFunc.cpp v1

--- GeneralFunc.cpp
+++ GeneralFunc.cpp
@@ -14,6 +14,14 @@ void SplitName(string ex, string& FirstName, string& LastName)
 	}
 }
 
+string remove_spaces(const string& s)
+{
+	int last = s.size() - 1;
+	while (last >= 0 && s[last] == ' ')
+		--last;
+	return s.substr(0, last + 1);
+}
+
 string SplitPassword(string str)
 {
 	string Pass;
@@ -38,4 +46,52 @@ bool in_array(string value, string array[], int n)
 		}
 	}
 	return false;
+}
+
+bool CheckSpecial(char c)
+{
+	string Special = "!@#$%^&*()/:_+-=<>?,.";
+	for (int i = 0; i <=Special.length(); i++)
+	{
+		if (c == Special[i])
+		{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+
+
+void GetDateTime(DateTime& DT)
+{
+	time_t now = time(0);
+	tm* ltm = localtime(&now);
+	DT.Year = 1900 + ltm->tm_year;//tm_year year since 1900
+	DT.Month = 1 + ltm->tm_mon;
+	DT.Day = ltm->tm_mday;
+	DT.T_hour = 1 + ltm->tm_hour;
+	DT.T_min = 1 + ltm->tm_hour;
+	DT.T_sec = 1 + ltm->tm_sec;
+}
+
+void DisplayDateTime(DateTime DT)
+{
+	cout << "\nYear: " << DT.Year << endl;
+	cout << "Month: " << DT.Month << endl;
+	cout << "Day: " << DT.Day << endl;
+	cout << "Time: " << DT.T_hour << ":";
+	cout << DT.T_min << ":";
+	cout << DT.T_sec << endl;
+}
+
+void SettingDateTime(DateTime& DT)
+{
+	cout << "Please enter Setting Date and Time.\n";
+	cout << "Enter Year: "; cin >> DT.Year;
+	cout << "Enter Month: "; cin >> DT.Month;
+	cout << "Enter Day: "; cin >> DT.Day;
+	cout << "Enter Hour Time: "; cin >> DT.T_hour;
+	cout << "Enter Minute Time:"; cin >> DT.T_min;
+	cout << "Enter second Time:"; cin >> DT.T_sec;
 }
