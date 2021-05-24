90a4c41334c7a608ffe205f430c7e9878bf13290
Nguyễn Tấn Thiện <tanthien225@gmail.com>
2021-05-24 23:08:23 +0700

    Update GeneralFunc.h v1

--- GeneralFunc.h
+++ GeneralFunc.h
@@ -16,6 +16,20 @@ string SplitPassword(string str);
 
 bool in_array(string value, string array[], int n);
 
+bool CheckSpecial(char c);
+
+struct DateTime
+{
+	int Year, Month, Day, T_hour, T_min, T_sec;
+};
+
+void GetDateTime(DateTime& DT);
+
+void DisplayDateTime(DateTime DT);
+
+void SettingDateTime(DateTime& DT);
+
+string remove_spaces(const string& s);
 
 #endif
 
